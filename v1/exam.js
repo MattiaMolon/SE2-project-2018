ce103d86ce507443738c0baa1bc3c238be10eed9
toniollimarta <marta.toniolli@studenti.unitn.it>
2018-12-02 18:13:28 +0100

    Revert "revert due to problems with origin/impl-test-marta"
    This reverts commit 21e8141abb66cb198e128bef765de9b9928faf49.

--- v1/exam.js
+++ v1/exam.js
@@ -7,9 +7,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000
 
 let exams = [
-            {id:1, taskgroup: null, startline: 24, deadline: 30, classes: null , teacher: "a"},
-            {id: 2, taskgroup: null, startline: 1, deadline: 15, classes: 5 , teacher: "b" },
-            {id: 3, taskgroup: null, startline: 5, deadline: 20, classes: 4, teacher: "c"}
+            {id:1, taskgroup: null, startline: 24, deadline: 30, classes: null},
+            {id: 2, taskgroup: null, startline: 1, deadline: 15, classes: 5},
+            {id: 3, taskgroup: null, startline: 5, deadline: 20, classes: 4}
             ]
 
 
@@ -37,15 +37,14 @@ app.post('/exams', (req, res) => {
     const start_line = req.body.startline
     const dead_line = req.body.deadline
     const classe = req.body.classes
-    const teach = req.body.teacher
     const new_id = exams.length +1
 
     //manca controllo. NON DEVE INSERIRE ID ALTRIMENTI LO SOVRASCRIVE
-    if(task_group==null || classe == null || dead_line ==null || teach == null){ //params required
+    if(task_group==null || classe == null || dead_line ==null){ //params required
         res.status(400) //Bad Request
         console.log('Params required cannot be null')
     } else {
-        const new_exam =  {id:new_id, taskgroup: task_group, startline: start_line, deadline: dead_line, classes: classe, teacher: teach} 
+        const new_exam =  {id:new_id, taskgroup: task_group, startline: start_line, deadline: dead_line, classes: classe} 
         exams.push(new_exam)
 
         res.json(exams)
@@ -66,7 +65,6 @@ app.put('/exams', (req, res) => {
     const start_line = req.body.startline
     const dead_line = req.body.deadline
     const classe = req.body.classes
-    const teach = req.body.teacher
 
     //params required  
     if(task_group === null){
@@ -84,31 +82,15 @@ app.put('/exams', (req, res) => {
         res.status(400) //Bad Request
         return;
     }
-    if(teach === null){ 
-        console.log('Bad Request')
-        res.status(400) //Bad Request
-        return;
-    }
 
     //manca controllo. NON DEVE ESSERE POSSIBILE MODIFICARE L'ID
 
     for(i=0; i< exams.length; i++){
         let ex = exams[i];
-        if(! (task_group == null)) {
-            ex.taskgroup = task_group;     
-        }
-        if(!(start_line == null)){
-            ex.startline = start_line;
-        }
-        if(!(dead_line == null)){
-            ex.deadline = dead_line;
-        }
-        if(!(classe == null)){
-            ex.classes=classe;
-        }
-        if(!(teach == null)){
-            ex.teacher = teach
-        }
+        ex.taskgroup = task_group;
+        ex.startline = start_line;
+        ex.deadline = dead_line;
+        ex.classes=classe;
     }
     res.status(200)
     res.send(exams)
@@ -174,7 +156,6 @@ app.put('/exams/:examId', (req, res) => {
     const start_line = req.body.startline
     const dead_line = req.body.deadline
     const classe = req.body.classes
-    const teach = req.body.teacher
 
     //params required
     if(task_group!=null){
@@ -189,9 +170,6 @@ app.put('/exams/:examId', (req, res) => {
 
     if(start_line!=null){
         exam.startline = start_line;
-    }
-    if(teach!=null){
-        exam.teacher = teach;
     }
 
     if(exam.error){ 
